585e10507fb91b6293d0e4d1550fd432e3474adb
lismgmk <lismgmk2@gmail.com>
2025-07-02 02:57:19 +0300

    Fix base path for ads-car repository

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/ads_transport/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ads-car/' : '/',
   server: {
     port: 3000,
     open: true,
